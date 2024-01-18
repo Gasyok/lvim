1d1a65f1492c84357ef3c0b0e923f208a97cc225
Gassyrbek Kosherbay <gas@Gassyrbeks-MacBook-Pro.local>
2024-01-18 14:58:55 +0500

    fix

--- lua/user/config.lua
+++ lua/user/config.lua
@@ -1,3 +1,6 @@
+vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+
 lvim.plugins = {
 	{ "github/copilot.vim" },
 	{
@@ -100,21 +103,26 @@ lvim.lang.htmldjango.lsp.setup = {
 	-- Additional configuration options specific to the html LSP
 }
 
-local ok, copilot = pcall(require, "copilot")
-if not ok then
-	return
-end
+-- lvim.g.copilot_no_tab_map = true
+-- lvim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
-copilot.setup({
-	suggestion = {
-		keymap = {
-			accept = "<c-l>",
-			next = "<c-j>",
-			prev = "<c-k>",
-			dismiss = "<c-h>",
-		},
-	},
-})
+-- lvim.g.copilot_assume_mapped = true
 
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
+-- local ok, copilot = pcall(require, "copilot")
+-- if not ok then
+-- 	return
+-- end
+
+-- copilot.setup({
+-- 	suggestion = {
+-- 		keymap = {
+-- 			accept = "<c-l>",
+-- 			next = "<c-j>",
+-- 			prev = "<c-k>",
+-- 			dismiss = "<c-h>",
+-- 		},
+-- 	},
+-- })
+
+-- local opts = { noremap = true, silent = true }
+-- vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
